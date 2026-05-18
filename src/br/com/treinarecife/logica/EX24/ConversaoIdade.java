9ba6072fe083919e8b357f9ac0ead1ec08b0d94c
Agusto César <negocios.augusto@gmail.com>
2026-05-18 07:52:44 -0300

    Criação da aula 24

---
+++ src/br/com/treinarecife/logica/EX24/ConversaoIdade.java
@@ -0,0 +1,18 @@
+package br.com.treinarecife.logica.EX24;
+
+public class ConversaoIdade {
+
+  public static void converterIdades(int idade) {
+
+    System.out.println(idade);
+
+  }
+
+  public static void main(String[] args) {
+    // Estudar meta espaços nos dois chates que deixei
+    // primeiro no google
+    ConversaoIdade.converterIdades(400);
+
+  }
+
+}
